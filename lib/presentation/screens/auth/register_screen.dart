e1d80f84ae4e69bd9c2fa0af6bb4180b7b33a5f9
Mohamed Heiba <mohamed222101223@ksiu.edu.eg>
2025-07-01 23:24:04 +0300

    Create authheader widget file for login and register

--- lib/presentation/screens/auth/register_screen.dart
+++ lib/presentation/screens/auth/register_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:the_grimoire/presentation/themes/colors.dart';
+import 'package:the_grimoire/app/themes/colors.dart';
+import 'package:the_grimoire/presentation/widgets/authheader.dart';
 
 class RegisterScreen extends StatefulWidget {
   const RegisterScreen({super.key});
@@ -13,36 +14,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.backgroundColor,
-      body: AuthHeader('Register', context),
+      body: authheader('Register', context),
     );
   }
-}
-
-Widget AuthHeader(String title, BuildContext context) {
-  return Padding(
-    padding: EdgeInsetsGeometry.all(25),
-    child: Column(
-      children: [
-        Image.asset(
-          'images/1.png',
-          width: MediaQuery.of(context).size.width * 0.6,
-          height: MediaQuery.of(context).size.height * 0.2,
-        ),
-        const SizedBox(height: 20),
-        Expanded(
-          child: Align(
-            alignment: Alignment.topLeft,
-            child: Text(
-              '$title',
-              style: TextStyle(
-                color: AppColors.textColor,
-                fontSize: 28,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
-        ),
-      ],
-    ),
-  );
 }
---
+++ lib/presentation/widgets/authheader.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:the_grimoire/app/themes/colors.dart';
+
+Widget authheader(String title, BuildContext context) {
+  return Padding(
+    padding: EdgeInsetsGeometry.all(25),
+    child: Column(
+      children: [
+        Image.asset(
+          'images/1.png',
+          width: MediaQuery.of(context).size.width * 0.6,
+          height: MediaQuery.of(context).size.height * 0.2,
+        ),
+        const SizedBox(height: 20),
+        Expanded(
+          child: Align(
+            alignment: Alignment.topLeft,
+            child: Text(
+              '$title',
+              style: TextStyle(
+                color: AppColors.textColor,
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ),
+      ],
+    ),
+  );
+}
