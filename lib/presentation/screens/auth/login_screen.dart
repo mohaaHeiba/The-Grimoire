bd228ddefacd15aa2ff1ec9d90b52640b25034ae
Mohamed Heiba <mohamed222101223@ksiu.edu.eg>
2025-07-03 01:13:21 +0300

    create login page

--- lib/presentation/screens/auth/login_screen.dart
+++ lib/presentation/screens/auth/login_screen.dart
@@ -1,1 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:the_grimoire/app/themes/colors.dart';
+import 'package:the_grimoire/presentation/widgets/authformfield.dart';
+import 'package:the_grimoire/presentation/widgets/authheader.dart';
+import 'package:the_grimoire/data/services/auth_service.dart';
+import 'package:the_grimoire/domain/controllers/auth_controller.dart';
 
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      resizeToAvoidBottomInset: true,
+      backgroundColor: AppColors.backgroundColor,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(
+                width: size.width,
+                height: 250,
+                child: authheader('Register', context),
+              ),
+              Container(
+                width: double.infinity,
+                padding: const EdgeInsets.all(24),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    AuthFormFields(
+                      emailController: _emailController,
+                      passwordController: _passwordController,
+
+                      showNameFields: true,
+                    ),
+                    const SizedBox(height: 24),
+                    SizedBox(
+                      height: 48,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color.fromARGB(255, 38, 8, 82),
+                          foregroundColor: AppColors.textColor,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(16),
+                          ),
+                          textStyle: const TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        onPressed: () {
+                          // Handle registration logic
+                        },
+                        child: const Text('Register'),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
